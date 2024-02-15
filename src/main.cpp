d4e3dda5024225334a0f7e1e3c92a9b66b411eab
Capeling <113705504+Capeling@users.noreply.github.com>
2024-02-15 16:06:55 +1030

    1.1.4

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,18 @@ class $modify(GJGarageLayer) {
 			return false;
 		auto winSize = CCDirector::sharedDirector()->getWinSize();
 
-		auto demonIcon = CCSprite::createWithSpriteFrameName("diffIcon_06_btn_001.png");
+		auto demonIcon = CCSprite::createWithSpriteFrameName("GJ_demonIcon_001.png");
 		demonIcon->setPosition({winSize.width - 18, winSize.height - 117});
 		demonIcon->setAnchorPoint({0.5, 0.5});
-		demonIcon->setScale({0.4});
+		demonIcon->setScale({0.5});
+		demonIcon->setID("demons-icon");
 
 		CCDictionary* stats = GameStatsManager::sharedState()->m_playerStats;
 		CCLabelBMFont* demonText = CCLabelBMFont::create(std::to_string(stats->valueForKey("5")->intValue()).c_str(), "bigFont.fnt");
-		demonText->setPosition({demonIcon->getPositionX() - 12, this->getChildByID("diamond-shards-label")->getPositionY() - 15});
+		demonText->setPosition(ccp(demonIcon->getPositionX() - 12, this->getChildByID("diamond-shards-label")->getPositionY() - 15));
 		demonText->setScale({0.34});
 		demonText->setAnchorPoint({1, 0.5});
+		demonText->setID("demons-label");
 
 		this->addChild(demonIcon);
 		this->addChild(demonText);
